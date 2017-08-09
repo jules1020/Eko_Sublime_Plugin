3773e14aacfdb41abecf543181f3a5eeb3356f74
Julian Smith De Niro <jsd410@nyu.edu>
2017-08-09 18:01:56 -0400

    replace all text feature, added docstrings

--- eko_sublime_plugin.py
+++ eko_sublime_plugin.py
@@ -1,7 +1,13 @@
+"""Import modules."""
 import sublime
 import sublime_plugin
 
 
 class EkoSublimePluginCommand(sublime_plugin.TextCommand):
+    """Eko sublime plugin class."""
+
     def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
+        """Run function."""
+        # self.view.insert(edit, 0, "Hello, World!")
+        allcontent = sublime.Region(0, self.view.size())
+        self.view.replace(edit, allcontent, 'Hello, World')
