041ec7a08e7bc08f619008ce579dca5bc89c76e1
Julian Smith De Niro <jsd410@nyu.edu>
2017-08-09 18:15:51 -0400

    I rock

--- eko_sublime_plugin.py
+++ eko_sublime_plugin.py
@@ -10,4 +10,4 @@ class EkoSublimePluginCommand(sublime_plugin.TextCommand):
         """Run function."""
         # self.view.insert(edit, 0, "Hello, World!")
         allcontent = sublime.Region(0, self.view.size())
-        self.view.replace(edit, allcontent, 'Hello, World')
+        self.view.replace(edit, allcontent, 'You rock Julian')
