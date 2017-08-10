38a3a3fb25df86aa0519183158cd93c8da596540
Julian Smith De Niro <jsd410@nyu.edu>
2017-08-10 19:22:55 -0400

    updated hints for each object based on 'kind' property

--- MetadataTransformer/metadatatransformer.py
+++ MetadataTransformer/metadatatransformer.py
@@ -6,8 +6,14 @@ def transform(metadata):
     for the_object in metadata:
         autocomplete = []
 
+        kind = ''
+        if the_object["kind"] == 'function':
+            kind = 'Method'
+        elif the_object["kind"] == 'member':
+            kind = 'Member'
+
         trigger = the_object["name"] + '\t' + 'Eko' + ' ' \
-            + the_object["memberof"] + ' ' + 'Method'
+            + the_object["memberof"] + ' ' + kind
 
         autocomplete.append(trigger)
         autocomplete.append(the_object["name"])
--- MetadataTransformer/test_metadatatransformer.py
+++ MetadataTransformer/test_metadatatransformer.py
@@ -5,18 +5,22 @@ import unittest
 class TestMetadataTransformer(unittest.TestCase):
     def test_objects(self):
         objects = [
-            {"name": "on", "memberof": "InterludePlayer"},
-            {"name": "update", "memberof": "Repository"},
-            {"name": "pluginInited", "memberof": "InterludePlayer"},
-            {"name": "once", "memberof": "Node"},
-            {"name": "trigger", "memberof": "Node"}]
+            {"name": "on", "memberof": "InterludePlayer", "kind": "function"},
+            {"name": "update", "memberof": "Repository", "kind": "function"},
+            {"name": "pluginInited", "memberof": "InterludePlayer",
+                "kind": "function"},
+            {"name": "once", "memberof": "Node", "kind": "function"},
+            {"name": "trigger", "memberof": "Node", "kind": "function"},
+            {"name": "version", "memberof": "InterludePlayer",
+                "kind": "member"}]
         result = metadatatransformer.transform(objects)
         expected_result = [
             ["on\tEko InterludePlayer Method", "on"],
             ["update\tEko Repository Method", "update"],
             ["pluginInited\tEko InterludePlayer Method", "pluginInited"],
             ["once\tEko Node Method", "once"],
-            ["trigger\tEko Node Method", "trigger"]]
+            ["trigger\tEko Node Method", "trigger"],
+            ["version\tEko InterludePlayer Member", "version"]]
         self.assertEqual(result, expected_result)
 
 if __name__ == "__main__":
