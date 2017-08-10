ba45499e7f7cd154508d2ad5bb39a00866a97ef3
Julian Smith De Niro <jsd410@nyu.edu>
2017-08-10 18:46:27 -0400

    optimized transform

--- MetadataTransformer/metadatatransformer.py
+++ MetadataTransformer/metadatatransformer.py
@@ -5,10 +5,11 @@ def transform(metadata):
     retval = []
     for the_object in metadata:
         autocomplete = []
-        memberof = the_object["memberof"]
-        trigger = the_object["name"]
-        trigger_final = trigger + '\t' + 'Eko' + ' ' + memberof + ' ' + 'Method'
-        autocomplete.append(trigger_final)
+
+        trigger = the_object["name"] + '\t' + 'Eko' + ' ' \
+        + the_object["memberof"] + ' ' + 'Method'
+
+        autocomplete.append(trigger)
         autocomplete.append(the_object["name"])
         retval.append(autocomplete)
     return retval
