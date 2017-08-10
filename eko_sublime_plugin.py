8ca3e2e6d96faa0ae64ac32aa54d07e1f957d5ed
Julian Smith De Niro <jsd410@nyu.edu>
2017-08-09 23:27:06 -0400

    added notes file, updated docstrings

--- eko_sublime_plugin.py
+++ eko_sublime_plugin.py
@@ -4,10 +4,10 @@ import sublime_plugin
 
 
 class EkoSublimePluginCommand(sublime_plugin.TextCommand):
-    """Eko sublime plugin class."""
+    """sublime_plugin.TextCommand Class."""
 
     def run(self, edit):
-        """Run function."""
+        """Called when the command is run."""
         # self.view.insert(edit, 0, "Hello, World!")
         allcontent = sublime.Region(0, self.view.size())
         self.view.replace(edit, allcontent, 'You rock Julian')
