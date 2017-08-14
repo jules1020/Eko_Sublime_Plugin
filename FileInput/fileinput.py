17c4477bb5a42216ac9304eb5370f5dd1ed2b3b9
Julian Smith De Niro <jsd410@nyu.edu>
2017-08-14 13:20:30 -0400

    made sure all 'memberof' values except 'urls' was capitalized

--- FileInput/fileinput.py
+++ FileInput/fileinput.py
@@ -3,19 +3,22 @@ import json
 
 def transform(metadata):
     retval = []
-    with open('autocomplete.json') as file_object:
+    with open("autocomplete.json") as file_object:
         metadata = json.load(file_object)
     for the_object in metadata:
         autocomplete = []
 
-        kind = ''
-        if the_object["kind"] == 'function':
-            kind = 'Method'
-        elif the_object["kind"] == 'member':
-            kind = 'Member'
+        kind = ""
+        for value in the_object:
+            if the_object["memberof"] != "urls":
+                memberof = the_object["memberof"].capitalize()
+        if the_object["kind"] == "function":
+            kind = "Method"
+        elif the_object["kind"] == "member":
+            kind = "Member"
 
-        trigger = the_object["name"] + '\t' + 'Eko' + ' ' \
-            + the_object["memberof"] + ' ' + kind
+        trigger = the_object["name"] + "\t" + "Eko" + " " \
+            + memberof + " " + kind
 
         autocomplete.append(trigger)
         autocomplete.append(the_object["name"])
