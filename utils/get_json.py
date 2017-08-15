11271af92c1c108901345ade3bf6a4552698fbb7
Julian Smith De Niro <julian@helloeko.com>
2017-08-15 14:45:54 -0400

    deleted some comments, GetJSON --> utils

--- utils/get_json.py
+++ utils/get_json.py
@@ -7,6 +7,5 @@ def url_to_json(metadata):
     """Load the JSON file from a URL."""
     with urllib.request.urlopen(metadata) as url:
         data = json.loads(url.read().decode())
-        # print(data)
         return (data)
-# url_to_json("https://developer.helloeko.com/api/autocomplete.json")
+
