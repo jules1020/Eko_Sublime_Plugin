0b937fdbf4725984d2e14f8ebbfdbc775818717a
Julian Smith De Niro <julian@helloeko.com>
2017-08-17 12:25:18 -0400

    `eko_sublime_plugin.py` loads eko json before `on_query_completions`

--- eko_sublime_plugin.py
+++ eko_sublime_plugin.py
@@ -8,12 +8,22 @@ from . import MetadataTransformer
 class EkoSublimePlugin(sublime_plugin.EventListener):
     """Sublime_plugin.EventListener."""
 
+    def __init__(self, *args, **kwargs):
+        """
+        Access the super of sublime_plugin.EventListener.
+
+        Load and transform JSON.
+        """
+        super().__init__()
+        self.transformed_json = \
+            MetadataTransformer.metadatatransformer.transform(
+                utils.get_json.url_to_json(
+                    "https://developer.helloeko.com/api/autocomplete.json"))
+
     def on_query_completions(self, view, prefix, locations):
         """
         Called whenever completions are to be presented to the user.
 
         The prefix is a unicode string of the text to complete.
         """
-        return (MetadataTransformer.metadatatransformer.transform(
-            utils.get_json.url_to_json(
-                "https://developer.helloeko.com/api/autocomplete.json")))
+        return self.transformed_json
