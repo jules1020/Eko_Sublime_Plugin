b2c89fd306fac3bfe6ea4e6a0a0a767c0ab5fb58
Julian Smith De Niro <julian@helloeko.com>
2017-08-17 16:03:34 -0400

    formating of nodes

--- FormatNodeID/format_node_id.py
+++ FormatNodeID/format_node_id.py
@@ -1,1 +1,10 @@
-format_node_id.py
+def transform_nodeid(node_id_array):
+    retval = []
+    for node_id in node_id_array:
+        query_completion = []
+        trigger = node_id + "\tProject Node"
+        completion = node_id
+        query_completion.append(trigger)
+        query_completion.append(completion)
+        retval.append(query_completion)
+    return retval
--- FormatNodeID/test_format_node_id.py
+++ FormatNodeID/test_format_node_id.py
@@ -8,9 +8,12 @@ class TestFormatNodeID(unittest.TestCase):
 
     def test_formatnodeids(self):
         """Function to test formating."""
-        node_ids = ['node_beginning_39j7h2', 'node_julian_j391jg']
-        result = format_node_id.format(node_ids)
-        expected_result = 
+        node_ids = ["node_beginning_39j7h2", "node_julian_j391jg"]
+        result = format_node_id.transform_nodeid(node_ids)
+        expected_result = [
+            ["node_beginning_39j7h2\tProject Node", "node_beginning_39j7h2"],
+            ["node_julian_j391jg\tProject Node", "node_julian_j391jg"]]
+        self.assertEqual(result, expected_result)
 
 if __name__ == "__main__":
     unittest.main()
